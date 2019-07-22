af5049cc99605869bf390062956096241ae3feed
isjin <isjin1@163.com>
2019-07-22 15:47:42 +0800

    update

--- update_security_group_inbound_policy.py
+++ update_security_group_inbound_policy.py
@@ -1,32 +1,30 @@
 from function.aws_ec2 import AWSEC2
 import json
 
+
+def read_file(path):
+    f = open(path, 'r')
+    data = f.read()
+    f.close()
+    data = json.loads(data)
+    return data
+
 app = AWSEC2()
+
 
 # data = app.ec2_security_group_describe('sg-0a97d9150f2dfb4a8')
 # print(data)
-sg_application_id = 'sg-08ce92b068895a67b'
-sg_application_inbound = {
-    "securitygroupid": sg_application_id,
-    "policy": [
-        {
-            "IpProtocol": "-1",
-            "IpRanges": [
-                {
-                    "CidrIp": "120.31.137.144/29",
-                    "Description": "New Bund guest wifi"
-                },
-                {
-                    "CidrIp": "192.168.1.0/24",
-                    "Description": "New Bund guest wifi"
-                }
-            ]
-        }
-    ]
-}
-sg_info=app.ec2_security_group_describe(sg_application_id)
-inboud_info={}
-inboud_info['policy']=sg_info['IpPermissions']
-inboud_info["securitygroupid"]=sg_application_id
-app.ec2_security_group_outbound_policies_revoke(inboud_info)
-app.ec2_security_group_inbound_policies_add(sg_application_inbound)
+sg_application_id = 'sg-0c7f9e00b8d1e18b2'
+sg_info = app.ec2_security_group_describe(sg_application_id)
+inboud_info = {}
+inboud_info['policy'] = sg_info['IpPermissions']
+inboud_info["securitygroupid"] = sg_application_id
+app.ec2_security_group_inbound_policies_revoke(inboud_info)
+
+sg_devopschain_application_inbound_path = 'config/devops_chain/sg_inbound_devopschain.txt'
+sg_devopschain_application_inbound = read_file(sg_devopschain_application_inbound_path)
+sg_devopschain_application_inbound['securitygroupid'] = sg_application_id
+try:
+    app.ec2_security_group_inbound_policies_add(sg_devopschain_application_inbound)
+except Exception as e:
+    print(e.__str__())
