4294a516d01f68e6f585a594bc2270e3b567bb1b
isjin <isjin1@163.com>
2019-08-15 13:10:41 +0800

    update

--- update_security_group_inbound_policy.py
+++ update_security_group_inbound_policy.py
@@ -14,17 +14,17 @@ app = AWSEC2()
 
 # data = app.ec2_security_group_describe('sg-0a97d9150f2dfb4a8')
 # print(data)
-sg_application_id = 'sg-0c7f9e00b8d1e18b2'
+sg_application_id = 'sg-0e5a63e0254abc056'
 sg_info = app.ec2_security_group_describe(sg_application_id)
 inboud_info = {}
 inboud_info['policy'] = sg_info['IpPermissions']
 inboud_info["securitygroupid"] = sg_application_id
 app.ec2_security_group_inbound_policies_revoke(inboud_info)
 
-sg_devopschain_application_inbound_path = 'config/devops_chain/sg_inbound_devopschain.txt'
-sg_devopschain_application_inbound = read_file(sg_devopschain_application_inbound_path)
-sg_devopschain_application_inbound['securitygroupid'] = sg_application_id
+sg_inbound_path = 'config/JLR/waf/sg_inbound-waf-cil-alb.txt'
+sg_inbound = read_file(sg_inbound_path)
+sg_inbound['securitygroupid'] = sg_application_id
 try:
-    app.ec2_security_group_inbound_policies_add(sg_devopschain_application_inbound)
+    app.ec2_security_group_inbound_policies_add(sg_inbound)
 except Exception as e:
     print(e.__str__())
