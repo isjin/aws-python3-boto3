93703756ed28c7989d3e779e0885db4c378f19df
isjin <isjin1@163.com>
2020-03-07 22:15:41 +0800

    update

--- lamdba/get_resource/get_vpc_resources.py
+++ lamdba/get_resource/get_vpc_resources.py
@@ -11,7 +11,7 @@ class GetResources(object):
     def __init__(self):
         self.ec2 = boto3.client('ec2')
         self.s3 = boto3.resource('s3')
-        # self.file_path = '/tmp/instances_information.txt'
+        # self.file_path = '/tmp/vpc_information.txt'
         self.file_path = 'vpc_information.txt'
         self.s3_bucket = 'services.information'
         self.s3_file_path = 'vpc_information.txt'
@@ -56,9 +56,11 @@ class GetResources(object):
         subnets_info = self.ec2_subnets_describe(filters)
         new_subnets_info = []
         for subnet_info in subnets_info:
+            # print(subnet_info)
             ec2_subnet_id = subnet_info['SubnetId']
             ec2_subnet_cidrblock = subnet_info['CidrBlock']
             # ec2_subnet_availability_zone = subnet_info['AvailabilityZone']
+            ec2_subnet_available_ip_address_count = subnet_info['AvailableIpAddressCount']
             ec2_subnet_name = None
             if "Tags" in subnet_info.keys():
                 for tag in subnet_info['Tags']:
@@ -69,16 +71,66 @@ class GetResources(object):
                     'SubnetId': ec2_subnet_id,
                     'SubnetName': ec2_subnet_name,
                     'CidrBlock': ec2_subnet_cidrblock,
+                    'Available IPv4 IPs': ec2_subnet_available_ip_address_count,
                     # 'AvailabilityZone': ec2_subnet_availability_zone,
                 }
             )
         return new_subnets_info
 
-    def get_nat_gateways_info(self):
-        pass
+    def ec2_nat_gateways_describe(self, filters):
+        response = self.ec2.describe_nat_gateways(
+            Filters=filters
+        )
+        return response['NatGateways']
 
-    def get_internet_gateways_info(self):
-        pass
+    def get_nat_gateways_info(self, vpcid):
+        filters = [
+            {
+                'Name': 'vpc-id',
+                'Values': [
+                    vpcid,
+                ]
+            },
+        ]
+        new_nat_gateways_info = []
+        nat_gateways_info = self.ec2_nat_gateways_describe(filters)
+        for nat_gateway_info in nat_gateways_info:
+            # print(nat_gateway_info)
+            ec2_nat_gateway_id = nat_gateway_info['NatGatewayId']
+            ec2_nat_gateway_private_ip = nat_gateway_info['NatGatewayAddresses'][0]['PrivateIp']
+            ec2_nat_gateway_public_ip = nat_gateway_info['NatGatewayAddresses'][0]['PublicIp']
+            new_nat_gateways_info.append(
+                {
+                    'NatGatewayId': ec2_nat_gateway_id,
+                    'NatGatewayPublicIp': ec2_nat_gateway_public_ip,
+                    'NatGatewayPrivateIp': ec2_nat_gateway_private_ip,
+                }
+            )
+        return new_nat_gateways_info
+
+    def ec2_internet_gateways_describe(self, filters):
+        response = self.ec2.describe_internet_gateways(
+            Filters=filters,
+        )
+        return response['InternetGateways']
+
+    def get_internet_gateways_info(self, vpcid):
+        filters = [
+            {
+                'Name': 'attachment.vpc-id',
+                'Values': [
+                    vpcid,
+                ]
+            },
+        ]
+        internet_gateway_info = self.ec2_internet_gateways_describe(filters)[0]
+        ec2_internet_gateway_id = internet_gateway_info['InternetGatewayId']
+        ec2_internet_gateway_name = None
+        if "Tags" in internet_gateway_info.keys():
+            for tag in internet_gateway_info['Tags']:
+                if tag['Key'] == 'Name':
+                    ec2_internet_gateway_name = tag['Value']
+        return ec2_internet_gateway_id, ec2_internet_gateway_name
 
     def ec2_route_tables_describe(self, filters):
         response = self.ec2.describe_route_tables(
@@ -98,17 +150,31 @@ class GetResources(object):
         ]
         route_tables_info = self.ec2_route_tables_describe(filters)
         for route_table_info in route_tables_info:
-            print(route_table_info)
+            # print(route_table_info)
             ec2_route_table_id = route_table_info['RouteTableId']
-            ec2_route_table_name=None
+            ec2_route_table_name = None
             if "Tags" in route_table_info.keys():
                 for tag in route_table_info['Tags']:
                     if tag['Key'] == 'Name':
                         ec2_route_table_name = tag['Value']
+            associated_subnets = []
+            ec2_route_table_associations = route_table_info['Associations']
+            if len(ec2_route_table_associations) > 0:
+                for association in ec2_route_table_associations:
+                    if "SubnetId" in association.keys():
+                        associated_subnets.append(association['SubnetId'])
+            routes = []
+            ec2_routes = route_table_info['Routes']
+            for ec2_route in ec2_routes:
+                del ec2_route['Origin']
+                del ec2_route['State']
+                routes.append(ec2_route)
             new_route_tables_info.append(
                 {
-                    'RouteTableId':ec2_route_table_id,
-                    'RouteTableName':ec2_route_table_name,
+                    'RouteTableId': ec2_route_table_id,
+                    'RouteTableName': ec2_route_table_name,
+                    'AssociatedSubnets': associated_subnets,
+                    'Routes': routes,
                 }
             )
         return new_route_tables_info
@@ -129,18 +195,23 @@ class GetResources(object):
                         ec2_vpc_name = tag['Value']
             subnets_info = self.get_subnets_info(ec2_vpc_id)
             route_tables_info = self.get_route_tables_info(ec2_vpc_id)
+            ec2_internet_gateway_id, ec2_internet_gateway_name = self.get_internet_gateways_info(ec2_vpc_id)
+            nat_gateways_info = self.get_nat_gateways_info(ec2_vpc_id)
             total_infos.append(
                 {
                     'VpcId': ec2_vpc_id,
                     'VpcName': ec2_vpc_name,
                     'CidrBlock': ec2_vpc_cidr,
+                    'InternetGatewaysId': ec2_internet_gateway_id,
+                    'InternetGatewaysName': ec2_internet_gateway_name,
                     'Subnets': subnets_info,
-                    'RouteTables': route_tables_info
+                    'RouteTables': route_tables_info,
+                    'NatGateways': nat_gateways_info,
                 }
             )
         # print(total_infos)
         self.write_file(self.file_path, total_infos)
-        # self.s3.meta.client.upload_file(self.file_path, self.s3_bucket, self.s3_file_path)
+        self.s3.meta.client.upload_file(self.file_path, self.s3_bucket, self.s3_file_path)
 
 
 if __name__ == '__main__':
